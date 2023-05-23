e80973d371f1b32206a0a1e5c5592f8dd643c504
Jorge Kamezawa <jor_kamezawa@hotmail.com>
2023-05-23 00:46:13 -0300

    first commit

---
+++ infra/src/main/java/com/comercio/lancamento/infra/LancamentoApplication.java
@@ -0,0 +1,15 @@
+package com.comercio.lancamento.infra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan("com.comercio.lancamento.*")
+@SpringBootApplication
+public class LancamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LancamentoApplication.class, args);
+	}
+
+}
